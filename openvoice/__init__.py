1c5511cd2f21f0b51d71993bdf5cac2052467cf2
naveen choudhary <naveen.choudhary@badbeat.com>
2024-11-23 18:45:17 +0530

    Add Install lib configs

---
+++ openvoice/__init__.py
@@ -0,0 +1,11 @@
+from openvoice.api import BaseSpeakerTTS, ToneColorConverter
+from openvoice import utils, commons
+
+__version__ = '0.1.0'
+
+__all__ = [
+    'BaseSpeakerTTS',
+    'ToneColorConverter',
+    'utils',
+    'commons'
+]
